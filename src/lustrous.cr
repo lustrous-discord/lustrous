06184614daa671d2ba29be0368da776cbc703e9e
Connor Scialdone <connor@connorjulian.org>
2018-10-30 16:47:19 -0600

    Create Lustrous::Bot to wrap API/Gateway

--- src/lustrous.cr
+++ src/lustrous.cr
@@ -1,5 +1,19 @@
+require "./network/*"
+
+# TODO: Finish gateway implementation (see network/gateway.cr)
+# TODO: Finish API implementation (see network/api.cr)
+# TODO: Add caching implementation
+# TODO: Create bot framework
 
 module Lustrous
   URL = "https://github.com/lustrous-discord/lustrous"
   VER = "0.1.0"
+end
+
+class Lustrous::Bot
+  def initialize(@token : String)
+    @api = API.new @token
+    @gateway = Gateway.new @token, @api
+    @gateway.connect
+  end
 end
