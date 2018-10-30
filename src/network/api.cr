2381ea101cd4ed356dcdb16b5129db7c7369fcfd
Connor Scialdone <connor@connorjulian.org>
2018-10-30 16:46:21 -0600

    Create basic API wrapper implementation

---
+++ src/network/api.cr
@@ -0,0 +1,43 @@
+require "../lustrous"
+require "crest"
+require "json"
+
+# TODO: Implement remaining API methods
+# TODO: Implement rate limit protection
+# TODO: Implement caching
+
+class Lustrous::API
+  def initialize(@token : String)
+    @discord = Crest::Resource.new("https://discordapp.com/api/v6") do |resource|
+      resource.headers.merge!({
+        "Authorization" => "Bot "+@token,
+        "User-Agent"    => "DiscordBot (#{Lustrous::URL}, #{Lustrous::VER})"
+      })
+    end
+  end
+
+  enum HttpMethod; GET, PUT, POST, PATCH, DELETE end
+  private def request(method : HttpMethod, path : String, args = {} of String => JSON::Any)
+    response = case method
+    when .put?     then @discord[path].put     form: args.to_json
+    when .post?    then @discord[path].post    form: args.to_json
+    when .patch?   then @discord[path].patch   form: args.to_json
+    when .get?     then @discord[path].get     params: args.transform_values &.to_s
+    when .delete?  then @discord[path].delete  params: args.transform_values &.to_s
+    else raise "Invalid method #{method} passed to Lustrous::API#request - this is a bug!"
+    end
+
+    return nil if response.status_code/100 != 2
+    JSON.parse(response.body)
+  end
+
+  private macro endpoint(name, method, path, *args)
+    def {{name.id}}({{*args}})
+      request(HttpMethod::{{method.id}}, {{path}},
+        {% if !args.empty? %} { {% for arg in args %} "{{arg.var}}" => {{arg.var}} {% end %} } {% end %})
+    end
+  end
+
+  endpoint get_gateway,     GET, "/gateway"
+  endpoint get_gateway_bot, GET, "/gateway/bot"
+end
