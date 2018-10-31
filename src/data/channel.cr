85234862380cd4d85cdbf7e4b340ac52dfb75486
Connor Scialdone <connor@connorjulian.org>
2018-10-31 10:32:12 -0600

    Abstract GuildChannel properties into module, minor tweaks and fixes

--- src/data/channel.cr
+++ src/data/channel.cr
@@ -5,45 +5,50 @@ abstract class BaseChannel
   include JSON::Serializable
 
   getter id   : UInt64
-  getter type : Type
+  getter type : ChannelType
   getter name : String? = nil
+end
 
-  enum Type
-    GUILD_TEXT, DM, GUILD_VOICE, GROUP_DM, GUILD_CATEGORY
-  end
+enum ChannelType
+  GUILD_TEXT, DM, GUILD_VOICE, GROUP_DM, GUILD_CATEGORY
 end
 
 abstract class TextChannel < BaseChannel
   include JSON::Serializable
 
-  @[JSON::Field(converter: Time::Format::ISO_8601_DATE_TIME)]
   getter last_pin_timestamp : Time? = nil
   getter last_message_id    : UInt64? = nil
 end
 
+module GuildChannel
+  macro included
+    getter guild_id              : UInt64
+    getter position              : Int32
+    getter parent_id             : UInt64? = nil
+    getter permission_overwrites : Array(PermissionOverwrite)? = nil
+  end
+end
+
 class GuildTextChannel < TextChannel
   include JSON::Serializable
-  @type = Type::GUILD_TEXT
+  @type = ChannelType::GUILD_TEXT
   
-  getter guild_id              : UInt64
-  getter position              : Int32
-  getter topic                 : String? = nil
-  getter nsfw                  : Bool?   = nil
-  getter parent_id             : UInt64? = nil
-  getter rate_limit_per_user   : Int32?    = nil
-  getter permission_overwrites : Array(PermissionOverwrite)? = nil
+  include GuildChannel
+  getter topic               : String? = nil
+  getter nsfw                : Bool?   = nil
+  getter rate_limit_per_user : Int32?  = nil
 end
 
 class DMChannel < TextChannel
   include JSON::Serializable
-  @type = Type::DM
+  @type = ChannelType::DM
 
   getter recipients : Array(Hash(String, JSON::Any))? = nil # TODO: Implement user type
 end
 
 class GroupDMChannel < DMChannel
   include JSON::Serializable
-  @type = Type::GROUP_DM
+  @type = ChannelType::GROUP_DM
 
   getter icon           : String? = nil
   getter owner_id       : UInt64? = nil
@@ -52,25 +57,16 @@ end
 
 class VoiceChannel < BaseChannel
   include JSON::Serializable
-  @type = Type::GUILD_VOICE
+  @type = ChannelType::GUILD_VOICE
 
-  getter guild_id              : UInt64
+  include GuildChannel
   getter bitrate               : Int32
-  getter position              : Int32
-  getter user_limit            : Int32?    = nil
-  getter parent_id             : UInt64? = nil
-  getter permission_overwrites : Array(PermissionOverwrite)? = nil
+  getter user_limit            : Int32? = nil
 end
 
 class ChannelCategory < BaseChannel
   include JSON::Serializable
-  @type = Type::GUILD_CATEGORY
+  @type = ChannelType::GUILD_CATEGORY
 
-  getter guild_id              : UInt64
-  getter position              : Int32
-  getter parent_id             : UInt64? = nil
-  getter permission_overwrites : Array(PermissionOverwrite)? = nil
+  include GuildChannel
 end
-
-alias GuildChannel = GuildTextChannel | VoiceChannel | ChannelCategory
-alias DM = DMChannel | GroupDMChannel
